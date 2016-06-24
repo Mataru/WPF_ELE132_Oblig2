055ba2a13d1f10b19e11014a14ef60fc571a969f
Mataru <mathias.angelivk@gmail.com>
2016-06-24 20:57:37 +0200

    fixed something

--- WPF_ELE132_Oblig2/App.xaml.cs
+++ WPF_ELE132_Oblig2/App.xaml.cs
@@ -1,12 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Windows;
-using WPF_ELE132_Oblig2.View;
-using WPF_ELE132_Oblig2.ViewModel;
+﻿using System.Windows;
 
 namespace WPF_ELE132_Oblig2
 {
